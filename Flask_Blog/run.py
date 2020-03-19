a1a3c6cdea8be871a436b2eca5d06b9057dd39bd
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2020-03-19 14:44:01 +0100

    Add render_template func + 2 posts

--- Flask_Blog/run.py
+++ Flask_Blog/run.py
@@ -1,15 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Laurent Echeverria',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': '18 Mars 2020'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': '19 Mars 2020'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts = posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(debug = True)
