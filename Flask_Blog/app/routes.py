4629c5356a7858eadfff2efeb391696584e7f772
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2020-03-22 17:44:58 +0100

    add img_file to param

--- Flask_Blog/app/routes.py
+++ Flask_Blog/app/routes.py
@@ -108,5 +108,9 @@ def logout():
 @login_required
 
 def account():
+    image_file = url_for('static', 
+                         filename = 'profile_pics/' + current_user.image_file)
+
     return render_template('account.html', 
-                           title = 'Account')
+                           title = 'Account',
+                           image_file = image_file)
