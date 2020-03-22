bea2c0a96f9a6d8e39b06181e9d90a8fdd90f7fc
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2020-03-22 13:44:38 +0100

    Add login_required func + login if check account without logged

--- Flask_Blog/app/__init__.py
+++ Flask_Blog/app/__init__.py
@@ -10,6 +10,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///config.db'
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 from app import routes
