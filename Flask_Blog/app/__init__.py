836c5113c8888f38cf8b82a47216c8c4bb1e9328
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2020-03-25 21:55:40 +0100

    Add files via upload

--- Flask_Blog/app/__init__.py
+++ Flask_Blog/app/__init__.py
@@ -18,10 +18,23 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'warning'
 
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_USE_TLS'] = True
+# -----------------------------------------------------------------------------
+# Flask Mail - Default Config
+# https://pythonhosted.org/Flask-Mail/
+#
+# app.config['MAIL_SERVER']         =   'localhost'
+# app.config['MAIL_PORT']           =   25
+# app.config['MAIL_USE_SSL']        =   False
+# app.config['MAIL_USE_TLS']        =   False
+# app.config['MAIL_USERNAME']       =   None
+# app.config['MAIL_PASSWORD']       =   None
+# app.config['MAIL_DEFAULT_SENDER'] =   None
+# -----------------------------------------------------------------------------
+
+app.config['MAIL_SERVER']   = 'smtp.gmail.com'
+app.config['MAIL_PORT']     = 587
+app.config['MAIL_USE_SSL']  = False
+app.config['MAIL_USE_TLS']  = True
 app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
 app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
 
