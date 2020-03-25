d57e289407cc4430e7f0312f2a9dc9dafff7db15
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2020-03-25 13:38:53 +0100

    Fix import app

--- Flask_Blog/app/models.py
+++ Flask_Blog/app/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from app import db, login_manager
+from app import db, login_manager, app
 from flask_login import UserMixin
 
 @login_manager.user_loader
