4081c078eed45f41aa3e2c9567ed8f2637a82518
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2020-03-25 21:12:58 +0100

    Ups mail server config

--- Flask_Blog/app/__init__.py
+++ Flask_Blog/app/__init__.py
@@ -18,8 +18,9 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'warning'
 
-app.config['MAIL_SERVER'] = 'smtp.google.com'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
 app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
