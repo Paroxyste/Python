228989f3c7f5a0157de75907b4a56d8ce3cef106
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2020-03-27 23:35:11 +0100

    Fix key + db

--- Flask_Blog/app/config.py
+++ Flask_Blog/app/config.py
@@ -1,17 +1,15 @@
-import os
-
 # -----------------------------------------------------------------------------
 # Server Config
 
 class Config :
     # app server
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALECHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = '448cb4ea09507a2e6bab663063e5fc58'
+    SQLALECHEMY_DATABASE_URI = 'sqlite:///config.db'
 
     # mail server
     MAIL_SERVER   = 'smtp.gmail.com'
     MAIL_PORT     = 587
     MAIL_USE_SSL  = False
     MAIL_USE_TLS  = True
-    MAIL_USERNAME = os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    MAIL_USERNAME = 'contact@admin.fr'
+    MAIL_PASSWORD = 'password'
