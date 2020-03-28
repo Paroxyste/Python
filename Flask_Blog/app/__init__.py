6b7041b039b0aab9a757fb8d99d4e9b376c6e060
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2020-03-28 10:34:28 +0100

    Add config to init

--- Flask_Blog/app/__init__.py
+++ Flask_Blog/app/__init__.py
@@ -1,4 +1,4 @@
-from app.config       import Config
+#from app.config       import Config
 from flask            import Flask
 from flask_bcrypt     import Bcrypt
 from flask_login      import LoginManager
@@ -20,15 +20,30 @@ mail = Mail()
 # -----------------------------------------------------------------------------
 # Create App
 
-def create_app(config_class  = Config):
+def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
 
-    # init components
+    # config server
+    app.config['SECRET_KEY'] = '448cb4ea09507a2e6bab663063e5fc58'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///config.db'
+
+    # init server
     bcrypt.init_app(app)
     db.init_app(app)
-    login_manager.init_app(app)
+
+    # config mail server
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config['MAIL_PORT'] = 587
+    app.config['MAIL_USE_SSL'] = False
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USERNAME'] = 'contact@admin.com'
+    app.config['MAIL_PASSWORD'] = 'password'
+
+    # init mail server
     mail.init_app(app)
+
+    # init login manager
+    login_manager.init_app(app)
 
     # import routes
     from app.main.routes  import main
