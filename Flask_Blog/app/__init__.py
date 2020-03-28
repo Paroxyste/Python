ace1080b5ed9d69f11d19a3a1b21893fe268a7d8
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2020-03-28 23:50:31 +0100

    Add instruction for server mail

--- Flask_Blog/app/__init__.py
+++ Flask_Blog/app/__init__.py
@@ -32,12 +32,15 @@ def create_app():
     db.init_app(app)
 
     # config mail server
-    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-    app.config['MAIL_PORT'] = 587
-    app.config['MAIL_USE_SSL'] = False
-    app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USERNAME'] = 'contact@admin.com'
-    app.config['MAIL_PASSWORD'] = 'password'
+    # Enable this option after logging to Gmail to make it work.
+    # -> https://www.google.com/settings/security/lesssecureapps
+
+    app.config['MAIL_SERVER']   = 'smtp.gmail.com'
+    app.config['MAIL_PORT']     = 587
+    app.config['MAIL_USE_SSL']  = False
+    app.config['MAIL_USE_TLS']  = True
+    app.config['MAIL_USERNAME'] = 'contact@gmail.com'
+    app.config['MAIL_PASSWORD'] = 'gmail_password'
 
     # init mail server
     mail.init_app(app)
