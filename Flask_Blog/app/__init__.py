f65a2cf6e86cf79a1e0f7db3fef4046930b6fd23
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2020-03-29 10:43:16 +0200

    Add errors

--- Flask_Blog/app/__init__.py
+++ Flask_Blog/app/__init__.py
@@ -1,60 +1,35 @@
-#from app.config       import Config
-from flask            import Flask
-from flask_bcrypt     import Bcrypt
-from flask_login      import LoginManager
-from flask_mail       import Mail
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+from flaskblog.config import Config
 
-# -----------------------------------------------------------------------------
-# Init App
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
-
 login_manager = LoginManager()
 login_manager.login_view = 'users.login'
-login_manager.login_message_category = 'warning'
-
+login_manager.login_message_category = 'info'
 mail = Mail()
 
-# -----------------------------------------------------------------------------
-# Create App
 
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
+    app.config.from_object(Config)
 
-    # config server
-    app.config['SECRET_KEY'] = '448cb4ea09507a2e6bab663063e5fc58'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///config.db'
-
-    # init server
-    bcrypt.init_app(app)
     db.init_app(app)
-
-    # config mail server
-    # Enable this option after logging to Gmail to make it work.
-    # -> https://www.google.com/settings/security/lesssecureapps
-
-    app.config['MAIL_SERVER']   = 'smtp.gmail.com'
-    app.config['MAIL_PORT']     = 587
-    app.config['MAIL_USE_SSL']  = False
-    app.config['MAIL_USE_TLS']  = True
-    app.config['MAIL_USERNAME'] = 'contact@gmail.com'
-    app.config['MAIL_PASSWORD'] = 'gmail_password'
-
-    # init mail server
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
     mail.init_app(app)
 
-    # init login manager
-    login_manager.init_app(app)
-
-    # import routes
-    from app.main.routes  import main
-    from app.posts.routes import posts
-    from app.users.routes import users
-
-    app.register_blueprint(main)
-    app.register_blueprint(posts)
+    from flaskblog.users.routes import users
+    from flaskblog.posts.routes import posts
+    from flaskblog.main.routes import main
+    from flaskblog.errors.handlers import errors
     app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+    app.register_blueprint(errors)
 
     return app
