1a98b8ae62710bb91ab00329d5a04d5a8feaaffb
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2020-05-13 20:17:36 +0000

    Upd routes

--- Flask App/eShop/shop/admin/routes.py
+++ Flask App/eShop/shop/admin/routes.py
@@ -1,9 +1,50 @@
-from flask import redirect, render_template, request, session, url_for
-from shop  import app, db
+from flask import flash, redirect, render_template, request, session, url_for
 
-@app.route('/')
+from .forms  import LoginForm, RegistrationForm
+from .models import User
+
+from shop  import app, bcrypt, db
+
+from shop.products.models import AddProduct, Brand, Category
+
+# ------------------------------------------------------------------------------
+# Admin
+
+@app.route('/admin')
 def home():
-    return 'Home Page'
+    products = AddProduct.query.all()
+
+    return render_template('admin/index.html',
+                           title    = 'Admin Page',
+                           products = products)
+
+# ------------------------------------------------------------------------------
+# Brands
+
+@app.route('/brands')
+def brands():
+    brands = Brand.query.order_by(Brand.id.desc()).all()
+
+    return render_template('admin/brands.html',
+                           title = 'Brands',
+                           brands = brands)
+
+# ------------------------------------------------------------------------------
+# Categories
+
+@app.route('/categories')
+def categories():
+    categories = Category.query.order_by(Category.id.desc()).all()
+
+    return render_template('admin/brands.html',
+                           title = 'Categories',
+                           categories = categories)
+
+# ------------------------------------------------------------------------------
+# Login
+
+# ------------------------------------------------------------------------------
+# Register
 
 @app.route('/register')
 def register():
