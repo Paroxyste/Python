2ae2fca01206bba46a043dbef3df8e9aef41799d
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2020-05-14 12:33:32 +0000

    Add login + register routes

--- Flask App/eShop/shop/admin/routes.py
+++ Flask App/eShop/shop/admin/routes.py
@@ -43,9 +43,54 @@ def categories():
 # ------------------------------------------------------------------------------
 # Login
 
+@app.route('login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if (form.validate_on_submit()):
+        user = User.query.filter_by(email = form.email.data).first()
+
+        if (user and bcrypt.check_password_hash(user.password, form.password.data)):
+            session['email'] = form.email.data
+
+            flash(f'Welcome { form.email.data } you are connected now',
+                  'success')
+
+            return redirect(url_for('admin'))
+        else:
+            flash('Wrong email and password',
+                  'danger')
+
+            return redirect(url_for('login'))
+    
+    return render_template('admin/login.html',
+                           title = 'Login Page',
+                           form  = form)
+
 # ------------------------------------------------------------------------------
 # Register
 
 @app.route('/register')
 def register():
-    return render_template('admin/register.html', title = 'Register')
+    form = RegistrationForm()
+
+    if (form.validate_on_submit()):
+        hash_password = bcrypt.generate_password_hash(form.password.data)
+
+        user = User(name     = form.name.data,
+                    username = form.username.data,
+                    email    = form.email.data,
+                    password = hash_password)
+
+        db.session.add(user)
+
+        flash(f'Welcome { form.name.data } ! Thanks for registering',
+              'success')
+
+        db.session.commit()
+
+        return redirect(url_for('login'))
+
+    return render_template('admin/register.html',
+                           title = 'Registration Page',
+                           form  = form)
