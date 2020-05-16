fc7e78c9fb47c375481a541719ef4ab3a2ab7d3c
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2020-05-16 17:23:13 +0200

    Add register + logout routes

--- Flask App/eShop/shop/customers/routes.py
+++ Flask App/eShop/shop/customers/routes.py
@@ -41,4 +41,46 @@ def customerLogin():
                            form = form)
 
 # -----------------------------------------------------------------------------
+# Customer : logout
+
+@app.routes('/customer/logout')
+def customerLogout():
+    logout_user()
+
+    return redirect(url_for('home'))
+
+# -----------------------------------------------------------------------------
+# Customer : register
+
+@app.route('/customer/register', methods = ['GET', 'POST'])
+def customerRegister():
+    form = CustomerRegisterForm()
+
+    if (form.validate_on_submit()):
+        hash_password = bcrypt.generate_password_hash(form.password.data)
+
+        register = Register(name     = form.name.data,
+                            username = form.username.data,
+                            email    = form.email.data,
+                            password = hash_password,
+                            country  = form.country.data,
+                            city     = form.city.data,
+                            contact  = form.contact.data,
+                            address  = form.adress.data,
+                            zipcode  = form.zipcode.data)
+        
+        db.session.add(register)
+
+        flash(f'Welcome { form.name.data } ! Thank You for registering',
+              'success')
+
+        db.session.commit()
+
+        return redirect(url_for('login'))
+
+    return render_template('customer/register.html',
+                           form = form)
+
+# -----------------------------------------------------------------------------
+# Order : get order
 
