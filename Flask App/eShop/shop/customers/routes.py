97fee8ab95d3d659c1ef48c5ec06aeb279da69de
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2020-05-16 17:54:55 +0200

    Add Order + getOrder routes

--- Flask App/eShop/shop/customers/routes.py
+++ Flask App/eShop/shop/customers/routes.py
@@ -10,7 +10,6 @@ from .model import CustomerOrder, Register
 import json
 import os
 import pdfkit
-import secrets
 
 # -----------------------------------------------------------------------------
 # Customer : login
@@ -43,7 +42,7 @@ def customerLogin():
 # -----------------------------------------------------------------------------
 # Customer : logout
 
-@app.routes('/customer/logout')
+@app.route('/customer/logout')
 def customerLogout():
     logout_user()
 
@@ -84,3 +83,70 @@ def customerRegister():
 # -----------------------------------------------------------------------------
 # Order : get order
 
+@app.route('/getorder')
+@login_required
+def getOrder():
+    if (current_user.is_authenticated):
+        customer_id = current_user.id
+        invoice     = os.urandom(10).hex()
+
+        try:
+            order = CustomerOrder(invoice     = invoice,
+                                  customer_id = customer_id,
+                                  orders      = session['ShoppingCart'])
+
+            db.session.add(order)
+            db.session.commit()
+
+            session.pop('ShoppingCart')
+
+            flash('Your order has been sent successfully',
+                  'success')
+
+            return redirect(url_for('orders', 
+                                    invoice = invoice))
+
+        except Exception as e:
+            print(e)
+
+            flash('Something went wrong while get order',
+                  'danger')
+
+            return redirect(url_for('getCart'))
+
+# -----------------------------------------------------------------------------
+# Order : create order
+
+@app.route('orders/<invoice>')
+@login_required
+def orders(invoice):
+    if (current_user.is_authenticated):
+        grandTotal  = 0
+        subTotal    = 0
+        customer_id = current_user.id
+        customer    = Register.query.filter_by(id = customer_id).first()
+        orders      = CustomerOrder.query.filter_by(customer_id = customer_id,
+                                                    invoice = invoice) \
+                                         .order_by(CustomerOrder.id.desc()) \
+                                         .first()
+
+        for _key, product in orders.orders.items():
+            discount   = (product['discount'] / 100) * float(product['price'])
+            subTotal  += float(product['price']) * int(product['quantity'])
+            subTotal  -= discount
+            tax        = ('%.2f' % (.06 * float(subTotal)))
+            grandTotal = float('%.2f' % (1.06 * subTotal))
+    
+    else:
+        return redirect(url_for(customerLogin))
+
+    return render_template('customer/order.html',
+                           invoice    = invoice,
+                           tax        = tax,
+                           subTotal   = subTotal,
+                           grandTotal = grandTotal,
+                           customer   = customer,
+                           orders     = orders)
+
+# -----------------------------------------------------------------------------
+# Order : make pdf order
