d017c3c153da3276284f0f52be96516d1219077e
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2020-05-17 11:25:36 +0200

    Add Register class

--- Flask App/eShop/shop/customers/models.py
+++ Flask App/eShop/shop/customers/models.py
@@ -16,23 +16,23 @@ def user_loader(user_id):
 
 class CustomerOrder(db.Model):
     id = db.Column(db.Integer,
-                   primary_key = True)
+                   primary_key=True)
 
     invoice = db.Column(db.String(20),
-                        unique   = True,
-                        nullable = False)
+                        unique=True,
+                        nullable=False)
 
     status = db.Column(db.String(20),
-                       default  = 'Pending',
-                       nullable = False)
+                       default='Pending',
+                       nullable=False)
 
     customer_id = db.Column(db.Integer,
-                            unique   = False,
-                            nullable = False)
+                            unique=False,
+                            nullable=False)
 
     date_created = db.Column(db.DateTime,
-                             default  = datetime.utcnow,
-                             nullable = False)
+                             default=datetime.utcnow,
+                             nullable=False)
 
     orders = db.Column(JsonEncodedDict)
 
@@ -56,5 +56,52 @@ class JsonEncodedDict(db.TypeDecorator):
             return {}
         else:
             return json.loads(value)
+
+# -----------------------------------------------------------------------------
+# Register
+
+class Register(db.Model, UserMixin):
+    id = db.Column(db.Integer,
+                   primary_key=True)
+
+    name = db.Column(db.String(50),
+                     unique=False)
+
+    username = db.Column(db.String(50),
+                         unique=True)
+
+    email = db.Column(db.String(50),
+                      unique=True)
+
+    password = db.Column(db.String(255),
+                         unique=False)
+
+    country = db.Column(db.String(50),
+                        unique=False)
+
+    city = db.Column(db.String(50),
+                     unique=False)
+
+    contact = db.Column(db.String(50),
+                        unique=False)
+
+    address = db.Column(db.String(50),
+                        unique=False)
+
+    zipcode = db.Column(db.String(50),
+                        unique=False)
+
+    profile = db.Column(db.String(255),
+                        unique=False,
+                        default='default.jpg')
+
+    date_created = db.Column(db.DateTime,
+                             nullable=False,
+                             default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Register %r>' % self.name
+
+# -----------------------------------------------------------------------------
 
 db.create_all()
