f2f6c32b7f81c29422ec3191e927aad96229c5c0
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2020-05-18 10:53:29 +0200

    Add brands routes

--- Flask App/eShop/shop/products/routes.py
+++ Flask App/eShop/shop/products/routes.py
@@ -26,15 +26,95 @@ def categories():
 # -----------------------------------------------------------------------------
 # Brand : add new brand
 
+@app.route('/addbrand.html', methods=['GET', 'POST'])
+def add_brand():
+    if (request.method == 'POST'):
+        get_brand = request.form.get('brand')
+        brand     = Brand(name=get_brand)
+
+        db.session.add(brand)
+
+        flash(f'The brand {get_brand} was added to your database',
+              'success')
+
+        db.session.commit()
+
+        return redirect(url_for('addbrand'))
+
+    return render_template('products/addbrand.html',
+                           title='Add Brand',
+                           brands='brands')
+
 # -----------------------------------------------------------------------------
 # Brand : delete brand
+
+@app.route('/deletebrand/<int:id>', methods=['GET', 'POST'])
+def delete_brand(id):
+    get_brand = Brand.query.get_or_404(id)
+
+    if (request.method == 'POST'):
+        db.session.delete(get_brand)
+
+        flash(f'The brand {get_brand.name} was deleted from your database',
+              'success')
+
+        db.session.commit()
+
+        return redirect(url_for('admin'))
+
+    flash(f'The brand {get_brand.name} can\'t be deleted from your database',
+          'warning')
+
+    return redirect(url_for('admin'))
 
 # -----------------------------------------------------------------------------
 # Brand : get brand id
 
+@app.route('/brand/<int:id>')
+def get_brand(id):
+    page      = request.args.get('page', 1, type=int)
+    get_brand = Brand.query.filter_by(id=id) \
+                           .first_or_404()
+    brand     = AddProduct.query.filter_by(brand=get_brand) \
+                                .paginate(page=page,
+                                          per_page=9)
+
+    return render_template('product/index.html',
+                           brand=brand,
+                           brands=brands(),
+                           categories=categories(),
+                           get_brand=get_brand)
+
 # -----------------------------------------------------------------------------
 # Brand : update brand
 
+@app.route('/updatebrand/<int:id>', methods=['GET', 'POST'])
+def update_brand():
+    if ('email' not in session):
+        flash('Login first please',
+              'danger')
+
+        return redirect(url_for('login'))
+
+    update_brand = Brand.query.get_or_404(id)
+    get_brand    = request.form.get('brand')
+
+    if (request.method == 'POST') :
+        update_brand.name = get_brand
+
+        flash(f'The brand {update_brand.name} was changed to {get_brand}',
+              'success')
+        
+        db.session.commit()
+
+        return redirect(url_for('brands'))
+    
+    get_brand = update_brand.name
+
+    return render_template('products/addbrand.html',
+                           title='Update brand',
+                           brands='brands',
+                           update_brand=update_brand)
 # -----------------------------------------------------------------------------
 # Categories : add new category
 
