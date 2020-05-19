adbc7a5e52a901e531b30e13bd69b469eec3508d
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2020-05-19 21:33:19 +0200

    Add Brand + Category class

--- Flask App/eShop/shop/products/models.py
+++ Flask App/eShop/shop/products/models.py
@@ -69,7 +69,27 @@ class AddProduct(db.Model):
 # -----------------------------------------------------------------------------
 # Brand
 
+class Brand(db.Model):
+    id = db.Column(db.Integer,
+                   primary_key=True)
+
+    name = db.Column(db.String(30),
+                     unique=True,
+                     nullable=False)
+
+    def __repr__(self):
+        return '<Brand %r>' % self.name
 
 # -----------------------------------------------------------------------------
 # Category
 
+class Category(db.Model):
+    id = db.Column(db.Integer,
+                   primary_key=True)
+
+    name = db.Column(db.String(30),
+                     unique=True,
+                     nullable=False)
+
+    def __repr__(self):
+        return '<Category %r>' % self.name
