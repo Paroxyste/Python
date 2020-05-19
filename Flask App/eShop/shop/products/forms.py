009bbf8f09bcbad2516e04bbaab79d2990804873
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2020-05-19 22:09:59 +0200

    Add AddProducts form

---
+++ Flask App/eShop/shop/products/forms.py
@@ -0,0 +1,41 @@
+from flask_wtf.file     import FileAllowed, FileField, FileRequired
+from wtforms.validators import (DataRequired, FloatField, Form, IntegerField,
+                                Length, SubmitField, StringField, TextAreaField)
+
+class AddProducts(Form):
+    name = StringField('Name',
+                       validators=[DataRequired(), 
+                                   Length(min=3, max=80)])
+
+    price = StringField('Price',
+                        validators=[DataRequired()])
+
+    discount = IntegerField('Discount',
+                            default=0)
+
+    stock = IntegerField('Stock',
+                         validators=[DataRequired(),
+                                     Length(min=1)])
+
+    colors = StringField('Colors',
+                         validators=[DataRequired(),
+                                     Length(min=3)])
+
+    description = TextAreaField('Description',
+                                validators=[DataRequired(),
+                                            Length(min=3)])
+
+    image_1 = FileField('Image 1',
+                        validators=[FileRequired(),
+                                    FileAllowed(['jpg', 'png', 'gif', 'jpeg']),
+                                    'Image only please'])
+
+    image_2 = FileField('Image 2',
+                        validators=[FileRequired(),
+                                    FileAllowed(['jpg', 'png', 'gif', 'jpeg']),
+                                    'Image only please'])
+
+    image_3 = FileField('Image 3',
+                        validators=[FileRequired(),
+                                    FileAllowed(['jpg', 'png', 'gif', 'jpeg']),
+                                    'Image only please'])
